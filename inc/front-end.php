47b387302f3c3ba86e57a15bf5ed61faf69d356b
Jay Snee <jay.snee@me.com>
2015-05-15 12:34:25 +0100

    added first stab at a widget front end

--- inc/front-end.php
+++ inc/front-end.php
@@ -4,6 +4,26 @@
   
   echo $before_title . $title . $after_title;
 
+  $args = array(
+    'posts_per_page'   => $num_trophies,
+    'orderby'          => 'date',
+    'order'            => 'DESC',
+    'post_type'        => 'trophy',
+    'post_status'      => 'publish',
+    'suppress_filters' => true 
+  );
+  $posts_array = get_posts( $args );
+
+
+
+  if ($posts_array->have_posts() ) : 
+    while ( have_posts() ) : the_post(); 
+      echo the_post();
+    endwhile;
+  endif; 
+
+
+  wp_reset_query();
   echo $after_widget;
 
 ?>
